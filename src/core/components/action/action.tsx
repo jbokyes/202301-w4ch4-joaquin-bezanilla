3d59afe9bd1768eceaccbfdb834ddb9c0b0eede8
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-09 21:06:22 +0100

    fixed display

--- src/core/components/action/action.tsx
+++ src/core/components/action/action.tsx
@@ -1,7 +1,10 @@
+import { Display } from "../display/display";
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 export function Action() {
   return (
     <>
+      <Display></Display>
       <span className="number"></span>
       <a href="#" className="call">
         Call
