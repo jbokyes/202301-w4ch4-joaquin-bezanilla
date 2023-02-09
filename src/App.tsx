71764207cd68234e3b4d28a05e1c253f2eede42f
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-09 18:54:28 +0100

    Added base components and its base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Info } from "./core/components/info/info";
+import { Keyboard } from "./core/components/keyboard/keyboard";
+import { Actions } from "./core/components/actions/actions";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="container">
+        <main className="phone">
+          <Info></Info>
+          <Keyboard></Keyboard>
+          <Actions></Actions>
+        </main>
+      </div>
+    </body>
   );
 }
 
---
+++ src/core/components/action/action.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+export function Action() {
+  return (
+    <>
+      <span className="number">667359961</span>
+      <a href="#" className="call">
+        Call
+      </a>
+      <a href="#" className="hang active">
+        Hang
+      </a>
+    </>
+  );
+}
